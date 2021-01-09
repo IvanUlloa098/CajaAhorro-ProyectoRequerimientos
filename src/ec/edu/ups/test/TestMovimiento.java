b09c02a832ae48303f065a201043d466d5b5c360
dbalarezo <dhbl250407@gmail.com>
2021-01-09 03:05:52 -0500

    Update TestMovimiento.java

--- src/ec/edu/ups/test/TestMovimiento.java
+++ src/ec/edu/ups/test/TestMovimiento.java
@@ -2,14 +2,54 @@ package ec.edu.ups.test;
 
 import junit.framework.TestCase;
 
-public class TestMovimiento extends TestCase {
+import static org.junit.Assert.assertEquals;
 
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-	protected void tearDown() throws Exception {
-		super.tearDown();
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ec.edu.ups.aporte_ahorros.*;
+import ec.edu.ups.gestion.*;
+import ec.edu.ups.socios.*;
+
+
+public class TestMovimiento {
+
+	private CuentaAhorros ca;
+	private Movimiento m;
+	private Movimiento me;
+	private TipoMovimiento tm;
+	private TipoMovimiento tme;
+	private Date d = new Date(121,0,8);
+	
+	
+	public void escenario()
+	{
+		tm =new TipoMovimiento();
+		tm.setId(1);
+		tm.setNombre("Retiro");
+		m = new Movimiento(1,d, 400, tm);
 	}
+			
+	@Test
+	public void testMovimientos(){
+		
+		// esperado
+		tme = new TipoMovimiento();
+		tme.setId(1);
+		tme.setNombre("Retiro");
+		
+		me = new Movimiento(1,d, 400, tme);
+		
+		escenario();
+		assertEquals(me, m);
+	
+	
+}
+ 
+
 
 }
