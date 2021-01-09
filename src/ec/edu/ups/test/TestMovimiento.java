9ab66ba030d0ecd8e87b2834a8f92c1890bc15ba
dbalarezo <dhbl250407@gmail.com>
2021-01-09 00:47:56 -0500

    Create TestMovimiento.java
    se hace un test junit movimiento

---
+++ src/ec/edu/ups/test/TestMovimiento.java
@@ -0,0 +1,15 @@
+package ec.edu.ups.test;
+
+import junit.framework.TestCase;
+
+public class TestMovimiento extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
