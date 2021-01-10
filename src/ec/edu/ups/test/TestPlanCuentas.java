65376c0f66c71588ca7a14d1c02eb939eb3c7838
Francisco <52508076+PelaezFrancisco@users.noreply.github.com>
2021-01-10 00:06:31 -0500

    JUnit-JPelaez

---
+++ src/ec/edu/ups/test/TestPlanCuentas.java
@@ -0,0 +1,70 @@
+package ec.edu.ups.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ec.edu.ups.creditos.SolicitudCredito;
+import ec.edu.ups.socios.CuentaAhorros;
+import ec.edu.ups.socios.PlanCuentas;
+import ec.edu.ups.socios.Socio;
+import ec.udu.ups.controlador.PlanCuentasController;
+
+public class TestPlanCuentas {
+
+	private Socio socio;
+	private CuentaAhorros cuentaAhorros;
+	private PlanCuentasController controller;
+	private boolean confirmacion = false;
+	private PlanCuentas plan;
+	private SolicitudCredito solicitud;
+	
+	@Before
+	public void setUp() {
+		controller = new PlanCuentasController();
+	}
+	
+	@After
+	public void tearDown(){
+	}
+	
+	@Test
+	public void testAgregarSolicitudCredito() {
+		socio = new Socio();
+		
+		socio.setId(1);
+		socio.setEstado('A');
+		
+		cuentaAhorros = new CuentaAhorros();
+		
+		cuentaAhorros.setCuentaId(1);
+		cuentaAhorros.setNumeroCta(1);
+		cuentaAhorros.setFechaCre(new Date());
+		cuentaAhorros.setCuentaAct('A');
+		cuentaAhorros.setSocio(socio);
+		
+		confirmacion = controller.agregarPlanCuentas(cuentaAhorros);
+		
+		assertTrue("exito", confirmacion);
+		
+	}
+	
+	public boolean agregarPlanCuentas(CuentaAhorros cuentaA) {
+		plan = new PlanCuentas();
+		int id = 1;
+		String tipo = "Tipo";
+		String descrip = "Descrip";
+		plan.setId(id);
+		plan.setTipo(tipo);
+		plan.setDescrip(descrip);
+		plan.setCuentaA(cuentaA);
+		
+		return true;
+	}
+	
+	
+}
