d1b90df5b00b1addd2e0f9d1d8b53866a888a9a6
IvanUlloa098 <iulloa@est.up.edu.ec>
2021-01-10 22:29:19 -0500

    Clase test

--- src/ec/edu/ups/controlador/Test.java
+++ src/ec/edu/ups/controlador/Test.java
@@ -5,21 +5,47 @@ import javax.persistence.Persistence;
 
 import ec.edu.ups.dao.CajaDAO;
 import ec.edu.ups.dao.DAOFactory;
+import ec.edu.ups.dao.RolDAO;
+import ec.edu.ups.dao.UsuarioDAO;
 import ec.edu.ups.gestion.Caja;
 import ec.edu.ups.gestion.Rol;
+import ec.edu.ups.gestion.Usuario;
 
 public class Test {
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		EntityManager em = Persistence.createEntityManagerFactory("CajaAhorro-ProyectoRequerimientos").createEntityManager();
+		EntityManager em = Persistence.createEntityManagerFactory("CajaAhorro-ProyectoRequerimientos").createEntityManager();	
+		RolDAO rolDAO;
+		CajaDAO cajaDao;
+		UsuarioDAO usuarioDAO;
+		Rol rol1, rol2;
+		Caja caja;
+		Usuario usuario;			
 		
-		Rol rol = DAOFactory.getFactory().getRolDAO();
+		//Creacion de dos roles para administrador y usuario corriente
+		rolDAO = DAOFactory.getFactory().getRolDAO();
+		rol1 = new Rol(1, "USUARIO");
+		rolDAO.create(rol1);
+		rol2 = new Rol(2, "ADMINISTRADOR");
+		rolDAO.create(rol2);
 		
-		CajaDAO cajaDao = DAOFactory.getFactory().getCajaDAO();
-		Caja caja= new Caja(0,"Calle Vieja y Elia Liu",400.50);
+		//Creacion de una caja
+		cajaDao = DAOFactory.getFactory().getCajaDAO();
+		caja= new Caja(0,"Calle Vieja y Elia Liu",400.50);
 		cajaDao.create(caja);
-
+		
+		//Creacion de dos usuarios
+		usuarioDAO = DAOFactory.getFactory().getUsuarioDAO();
+		usuario = new Usuario(1, "usuario1", "1234", rol1, caja);
+		usuarioDAO.create(usuario);
+		
+		usuarioDAO = DAOFactory.getFactory().getUsuarioDAO();
+		usuario = new Usuario(2, "usuario2", "4321", rol2, caja);
+		usuarioDAO.create(usuario);
+		
+		
 	}
 
 }
